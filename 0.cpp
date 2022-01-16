849a2da3674f3459bef620d73010c74760ffc8aa
Zhuravskaya Margarita <92183963+Zhuravskaya167@users.noreply.github.com>
2022-01-17 01:49:34 +0300

    Add files via upload

--- 0.cpp
+++ 0.cpp
@@ -1,46 +1,90 @@
-//Вариант 7: int, avg
+﻿//Вариант 7: int, avg
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
+#include <cstdlib>
 using namespace std;
 
-int* generate_array(int siz) {
-    int* array = new int[siz];
-    cout << "Current array elements: ";
-    srand(time(NULL));
-    for (int i = 0; i < siz; i++) {
-        array[i] = rand() % 100;
-        printf("%d ", array[i]);
+/*!
+Функция установки размера массива
+*/
+int getSize();
+
+/*!
+Функция заполнения массива радомными значениями
+Принимает на вход: размер массива, указатель на массив
+*/
+void arrayCreation(const int size, int* arr);
+
+/*!
+Функция вывода массива на экран
+Принимает на вход: размер массива, указатель на массив
+*/
+void printArray(const int size, int* arr);
+
+/*!
+Функция вывода среднего арифметического массива
+Принимает на вход: размер массива, указатель на массив
+*/
+double average(const int size, int* arr);
+
+/*
+Главная функция
+*/
+int main()
+{
+    const int size = getSize();
+    int* arr = new int[size];
+    arrayCreation(size, arr);
+    cout << "Data array: " << "\n";
+    printArray(size, arr);
+    cout << "\nResult: " << "\n";
+    cout << average(size, arr);
+    
+    if (arr != nullptr)
+    {
+        delete[] arr;
+        arr = nullptr;
     }
-    return array;
 }
 
-int summ(int siz, int *arr) {
+int getSize()
+{
+    int size;
+    cout << "Enter the size of the array: ";
+    while (true) 	// Проверка на положительный размер массива
+    {
+        cin >> size;
+        if (size < 0)
+        {
+            cout << "Enter a positive number of elements: ";
+        }
+        else return size;
+    }
+}
+
+
+void arrayCreation(const int size, int* arr)
+{
+    srand(clock());
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand();
+    }
+}
+
+
+void printArray(const int size, int* arr)
+{
+    for (int i = 0; i < size; i++) {
+        cout << i + 1 << ": " << arr[i] << "\n";
+    }
+}
+
+
+double average(const int size, int* arr)
+{
     int sum = 0;
-    for (int i = 0; i < siz; i++) {
+    for (int i = 0; i < size; i++) {
         sum = sum + arr[i];
     }
-    return sum;
-}
-
-int main() {
-    int size = 0;
-    cout << "Enter a size of array: ";
-    cin >> size;
-
-
-    int* A = generate_array(size);
-    int sum = summ(size, A);
-
     const double average = static_cast<double>(sum) / size;
-
-    cout << "\nResult: " << average;
-
-    if (A != nullptr)
-    {
-        delete[] A;
-        A = nullptr;
-    }
+    return average;
 }
